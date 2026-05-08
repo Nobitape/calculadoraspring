26332a8254efd74d2296e8fd051ca319b8264555
martinxguajardo[1;8q[1;7r[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D <martinxguajardo[1;8q[1;7r[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D@gmail.com>
2026-05-08 17:19:25 -0400

    Me falto añadir esta wea xd

--- src/test/java/cl/usm/calculadoraspring/services/CalculadoraServiceTest.java
+++ src/test/java/cl/usm/calculadoraspring/services/CalculadoraServiceTest.java
@@ -1,46 +1,48 @@
 package cl.usm.calculadoraspring.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraServiceTest {
 
+    CalculadoraService calculadoraService;
+
+    @BeforeEach
+    void setUp(){
+        calculadoraService = new CalculadoraService();
+    }
+
     @Test
     void calcular_SumaOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         double resultado = calculadoraService.calcular("+", 1, 2);
         assertEquals(3, resultado);
     }
 
     @Test
     void calcular_RestaOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         double resultado = calculadoraService.calcular("-", 2, 1);
         assertEquals(1, resultado);
     }
 
     @Test
     void calcular_MultiplicacionOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         double resultado = calculadoraService.calcular("*", 2, 1);
         assertEquals(2, resultado);
     }
     @Test
     void calcular_SumaFailed() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         double resultado = calculadoraService.calcular("-", 1, 2);
         assertNotEquals(3, resultado);
     }
     @Test
     void calcular_DivisionOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         double resultado = calculadoraService.calcular("/", 4, 2);
         assertEquals(2, resultado);
     }
     @Test
     void calcular_DivisionNoOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         Exception ex = assertThrows(NumberFormatException.class, ()->{
            double resultado = calculadoraService.calcular("/",5,0);
         });
@@ -48,7 +50,6 @@ class CalculadoraServiceTest {
     }
     @Test
     void calcular_OperacionNoOk() {
-        CalculadoraService calculadoraService = new CalculadoraService();
         Exception ex = assertThrows(NumberFormatException.class, ()->{
             double resultado = calculadoraService.calcular("$",5,0);
         });
