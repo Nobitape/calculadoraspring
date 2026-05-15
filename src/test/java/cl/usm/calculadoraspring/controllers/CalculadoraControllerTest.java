75039cad6c3477154a055ace69dc30e2d870e9f6
martinxguajardou <martinxguajardou@gmail.com>
2026-05-15 16:34:58 -0400

    Add Controller Tests

---
+++ src/test/java/cl/usm/calculadoraspring/controllers/CalculadoraControllerTest.java
@@ -0,0 +1,63 @@
+package cl.usm.calculadoraspring.controllers;
+
+import cl.usm.calculadoraspring.entities.CalculadoraRequest;
+import cl.usm.calculadoraspring.services.CalculadoraService;
+import org.apache.coyote.BadRequestException;
+import org.apache.coyote.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CalculadoraControllerTest {
+
+    CalculadoraController calculadoraController;
+
+    @Mock
+    CalculadoraService calculadoraService;
+
+    @BeforeEach
+    void setUp(){
+        this.calculadoraController = new CalculadoraController(calculadoraService);
+    }
+
+    @Test
+    void calcular_sumaOk() {
+        CalculadoraRequest request = new CalculadoraRequest();
+        request.setN1(1.0);
+        request.setN2(2.0);
+        request.setOperation("+");
+        when(calculadoraService.calcular(anyString(),anyDouble(), anyDouble())).thenReturn(1.0);
+        ResponseEntity<Object> responseEntity = this.calculadoraController.calcular(request);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void calcular_sumaNoOk() {
+        CalculadoraRequest request = new CalculadoraRequest();
+        request.setN1(4.0);
+        request.setN2(2.0);
+        request.setOperation("+");
+        when(calculadoraService.calcular(anyString(),anyDouble(), anyDouble())).thenThrow(new NullPointerException());
+        ResponseEntity<Object> responseEntity = this.calculadoraController.calcular(request);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+    }
+    @Test
+    void calcular_BadRequest() {
+        CalculadoraRequest request = new CalculadoraRequest();
+        request.setN1(2.0);
+        request.setN2(1.0);
+        request.setOperation("$");
+        when(calculadoraService.calcular(anyString(),anyDouble(), anyDouble())).thenThrow(new NumberFormatException());
+        ResponseEntity<Object> responseEntity = this.calculadoraController.calcular(request);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+}
--- src/test/java/cl/usm/calculadoraspring/services/CalculadoraServiceTest.java
+++ src/test/java/cl/usm/calculadoraspring/services/CalculadoraServiceTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculadoraServiceTest {
+class
+CalculadoraServiceTest {
 
     CalculadoraService calculadoraService;
 
